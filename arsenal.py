650bff779bb1dd0edde7de546460003e2d42e413
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-04-05 14:45:21 -0700

    Created remaining models in arsenal + not emotionally ready to test out the place_ship logic yet

--- arsenal.py
+++ arsenal.py
@@ -2,5 +2,20 @@ from ship import Ship
 
 class Destroyer(Ship):
     def __init__(self):
-        super().__init__(2, "destroyer")
+        super().__init__(2, "Destroyer")
         
+class Submarine(Ship):
+    def __init__(self):
+        super().__init__(3, "Submarine")
+
+class Battleship_1(Ship):
+    def __init__(self):
+        super().__init__(4, "Battleship")
+
+class Battleship_2(Ship):
+    def __init__(self):
+        super().__init__(4, "Battleship")
+
+class Destroyer(Ship):
+    def __init__(self):
+        super().__init__(5, "Aircraft Carrier")
