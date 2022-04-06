2fa734715fdec5cbcdd854f7ac903c943f303fea
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-04-06 11:34:11 -0700

    Trimmed it down a bit to make it easier to read

--- battle.py
+++ battle.py
@@ -2,7 +2,7 @@ from . import battlefield
 
 class Battle:
     def __init__(self):
-        self.p1field = battlefield.Player1_Field()
-        self.p2field = battlefield.Player2_Field()
-        self.p1select = battlefield.Player1_Select()
-        self.p2select = battlefield.Player2_Select()
+        self.p1field = battlefield.PlayerField()
+        self.p2field = battlefield.PlayerField()
+        self.p1select = battlefield.PlayerSelect()
+        self.p2select = battlefield.PlayerSelect()
--- battlefield.py
+++ battlefield.py
@@ -2,22 +2,12 @@ import string
 from field import Field
 
 # The player's boards with ships on them
-class Player1_Field(Field):
-    def __init__(self):
-        super().__init__()
-        self.initialize = self.set_up_ships()
-
-class Player2_Field(Field):
+class PlayerField(Field):
     def __init__(self):
         super().__init__()
         self.initialize = self.set_up_ships()
 
 # What the players choose from
-class Player1_Select(Field):
+class PlayerSelect(Field):
     def __init__(self):
         super().__init__()
-
-class Player2_Select(Field):
-    def __init__(self):
-        super().__init__()
-
